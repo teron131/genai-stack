46b2217678bc1f81e4513bf9eed273072cb4bb03
teron131 <t3ron.ay@gmail.com>
2023-12-19 06:16:52 +0000

    Testbench streamlit

--- testbench.Dockerfile
+++ testbench.Dockerfile
@@ -16,4 +16,8 @@ COPY testbench.py .
 COPY chains.py .
 COPY utils.py .
 
-ENTRYPOINT ["python", "testbench.py"]
+EXPOSE 8506
+
+HEALTHCHECK CMD curl --fail http://localhost:8506/_stcore/health
+
+ENTRYPOINT ["streamlit", "run", "testbench.py", "--server.port=8506", "--server.address=0.0.0.0"]
--- testbench.py
+++ testbench.py
@@ -1,8 +1,10 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores.chroma import Chroma
+from langchain_core.documents.base import Document
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.prompts.chat import (
@@ -44,40 +46,38 @@ def configure_qa_rag_chroma_chain_test(llm, embeddings, general_system_template)
 
 # Define the system message templates
 general_system_template_baseline = "{summaries}"
-general_system_template_original = """
-    Use the following pieces of context to answer the question at the end.
-    The context contains question-answer pairs and their links from Stackoverflow.
-    Make sure to rely on information from the answers and not on questions to provide accuate responses.
-    ----
-    {summaries}
-    ----
-    Generate concise answers with references sources section of links to 
-    relevant StackOverflow questions only at the end of the answer.
-    DO NOT include the keywords "Question:", "Answer:", "Score:", "Link:" in your answer.
-    """
-general_system_template_with_instructions = """
-    Use the following pieces of context to answer the question at the end.
-    The context contains question-answer pairs in the following formmat:
-        Question: title
-        content
-        Answer: content
-        Score: score
-        Link: link
-    Make sure to rely on information from the answers and not on questions to provide accuate responses.
-    When you find particular answer in the context useful, make sure to cite it in the answer using the link.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    ----
-    {summaries}
-    ----
-    Each answer you generate should contain a section at the end of links to 
-    Stackoverflow questions and answers you found useful, which are described under Source value.
-    You can only use links to StackOverflow questions that are present in the context and always
-    add links to the end of the answer in the style of citations.
-    Generate concise answers with references sources section of links to 
-    relevant StackOverflow questions only at the end of the answer.
-"""
+general_system_template_original = """Use the following pieces of context to answer the question at the end.
+The context contains question-answer pairs and their links from Stackoverflow.
+You should prefer information from accepted or more upvoted answers.
+Make sure to rely on information from the answers and not on questions to provide accuate responses.
+When you find particular answer in the context useful, make sure to cite it in the answer using the link.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----
+{summaries}
+----
+Each answer you generate should contain a section at the end of links to 
+Stackoverflow questions and answers you found useful, which are described under Source value.
+You can only use links to StackOverflow questions that are present in the context and always
+add links to the end of the answer in the style of citations.
+Generate concise answers with references sources section of links to 
+relevant StackOverflow questions only at the end of the answer."""
+general_system_template_custom = """Use the following pieces of context to answer the question at the end.
+The context contains question-answer pairs and their links from Stackoverflow.
+Make sure to rely on information from the answers and not on questions to provide accuate responses.
+When you find particular answer in the context useful, make sure to cite it in the answer using the link.
+----
+{summaries}
+----
+Do not include the keyword "Answer:".
+Each answer you generate should contain a section at the end of links to 
+Stackoverflow questions and answers you found useful, which are described under Source value.
+You can only use links to StackOverflow questions that are present in the context and always
+add links to the end of the answer in the style of citations.
+Generate concise answers with references sources section of links to 
+relevant StackOverflow questions only at the end of the answer."""
 
 # Prepare the environment
+st.title("RAG Model Testbench")
 load_dotenv()
 url = os.getenv("NEO4J_URI")
 username = os.getenv("NEO4J_USERNAME")
@@ -87,46 +87,53 @@ embeddings = OpenAIEmbeddings()
 neo4j_graph = Neo4jGraph(url=url, username=username, password=password)
 chromadb = Chroma(persist_directory="data_chroma", embedding_function=embeddings)
 
-# Configure the RAG model with ChromaDB baseline prompt
-rag_chroma_chain_baseline = configure_qa_rag_chroma_chain_test(
-    llm=llm, embeddings=embeddings, general_system_template=general_system_template_baseline
-)
+# Streamlit interface for question input
+question = st.text_input("Enter your question", "Is there any way to import the data to `neo4j` desktop")
 
-# Configure the RAG model with ChromaDB and additional instructions prompt
-rag_chroma_chain_test = configure_qa_rag_chroma_chain_test(
-    llm=llm, embeddings=embeddings, general_system_template=general_system_template_original
-)
+# Add a button for accepting the question
+if st.button('Submit'):
+    # Check if the question has been entered and fetch answers
+    if question and 'retrieved_docs' not in st.session_state:
+        query_vector = embeddings.embed_query(question)
+        st.session_state['retrieved_docs'] = chromadb.similarity_search_by_vector(query_vector, k=2)
+        
+        # Configure and get the answer from the RAG model with Neo4j
+        rag_chain_neo4j = configure_qa_rag_chain(
+            llm, embeddings, embeddings_store_url=url, username=username, password=password
+        )
+        st.session_state['neo4j_answer'] = rag_chain_neo4j({"question": question})["answer"]
+        
+        # Configure and get answer for Chroma with the baseline prompt
+        rag_chroma_chain_baseline = configure_qa_rag_chroma_chain_test(
+            llm=llm, embeddings=embeddings, general_system_template=general_system_template_baseline
+        )
+        st.session_state['baseline_prompt_answer'] = rag_chroma_chain_baseline({"question": question})["answer"]
 
-# Define a question
-question = "Is there anyway to import the data to `neo4j` desktop"
+# Display the answers from Neo4j and Chroma with the baseline prompt if available
+### Note: There are some problems ins streamlit, so it just means that 'retrieve_docs' is initiated, but not stored
+if 'retrieved_docs' in st.session_state:
+    for doc in st.session_state['retrieved_docs']:
+        st.write(f"Document: {doc[0]}")
+        st.write(f"Score: {doc[1]}")
 
-# Simulate vector search
-query_vector = embeddings.embed_query(question)
-retrieved_docs = chromadb.similarity_search_by_vector_with_relevance_scores(query_vector, k=2)
-for doc in retrieved_docs:
-    print(doc[0])
-    print(doc[1])
+    # Display answers for Neo4j and the baseline prompt
+    st.subheader("RAG model with Neo4j")
+    st.write(st.session_state['neo4j_answer'])
 
-print("-"*100)
+    st.subheader("RAG model with Chroma and baseline prompt (baseline)")
+    st.write(st.session_state['baseline_prompt_answer'])
 
-# Get the answer from the baseline RAG model
-print("## RAG model with ChromaDB baseline prompt ##")
-print(rag_chroma_chain_baseline({"question": question})["answer"])
+    # Streamlit interface for custom prompt input
+    prompt = st.text_area("Enter your custom prompt", general_system_template_custom, height=200)
 
-print("-"*100)
+    # Button to trigger the process for custom prompt
+    if st.button('Get Answer with Custom Prompt'):
+        with st.spinner('Fetching answers...'):
+            # Configure the RAG model with Chroma with the custom prompt
+            rag_chroma_chain_custom = configure_qa_rag_chroma_chain_test(
+                llm=llm, embeddings=embeddings, general_system_template=prompt
+            )
 
-# Get the answer from the RAG model with additional instructions
-print("## RAG model with additional instructions prompt ##")
-print(rag_chroma_chain_test({"question": question})["answer"])
-
-
-# Configure the RAG model with Neo4j
-rag_chain = configure_qa_rag_chain(
-    llm, embeddings, embeddings_store_url=url, username=username, password=password
-)
-
-print("-"*100)
-
-# Get the answer from the baseline RAG model
-print("## RAG model with Neo4j ##")
-print(rag_chain({"question": question})["answer"])
+            # Get the answer from the RAG model with Chroma and custom prompt
+            st.subheader("RAG model with Chroma and custom prompt")
+            st.write(rag_chroma_chain_custom({"question": question})["answer"])
